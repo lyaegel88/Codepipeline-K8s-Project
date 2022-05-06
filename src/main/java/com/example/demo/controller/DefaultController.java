78102792cc1e105417ab569733a686a311a930e9
Luke Yaegel <luke.yaegel@gmail.com>
2022-05-05 22:37:05 -0400

    added new endpoint

--- src/main/java/com/example/demo/controller/DefaultController.java
+++ src/main/java/com/example/demo/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class DefaultController {
     @RequestMapping(path = {"","/"})
     public String getHello(){
         return "Hello World!";
+    }
+
+    @GetMapping("/luke")
+    public String getLuke(){
+        return "Hey, this is Luke";
     }
 }
